c7f5275cc1aa911633317b763d5654d7bcbb45ee
aostn <astngyn@gmail.com>
2022-01-20 16:36:21 -0800

    Updated modules to have bg imgs

--- src/Components/module/module.jsx
+++ src/Components/module/module.jsx
@@ -6,9 +6,11 @@ const Modules = () =>{
         <div>
             <header className="title">Our Most Popular Learning Modules</header>
             <ul className="itemsList">
-                <li className="item"><a href="#">Module 1</a></li>
-                <li className="item"><a href="#">Module 2</a></li>
-                <li className="item"><a href="#">Module 3</a></li>
+                <div className="item">
+                    <li className="item1"><a href="#"><h3>Module 1:</h3> Reading Charts</a></li>
+                    <li className="item2"><a href="#"><h3>Module 2:</h3> Options</a></li>
+                    <li className="item3"><a href="#"><h3>Module 3:</h3> Short Squeeze</a></li>
+                </div>
             </ul>
         </div>
         
