00c492f0cefc4dad9fdf9a84a62368feee1c40d6
aostn <astngyn@gmail.com>
2022-02-21 16:26:53 -0800

    Reupload

--- src/ModulesPage/index.jsx
+++ src/ModulesPage/index.jsx
@@ -1,23 +1,127 @@
-import react from "react";
+import react, {useState, useEffect} from "react";
 import './index.css'
+
+const TableOfContents = () =>{
+    const [activeModule, setActiveModule] = useState(0);
+
+    const subtopics = () => {
+
+        switch(activeModule){
+            case 1:
+                return (
+                    <ul>
+                        <li>Highs</li>
+                        <li>Lows</li>
+                    </ul>
+                );
+                
+
+            case 2:
+                return(
+                    <ul>
+                        <li>Stocks are cool</li>
+                        <li>Dips</li>
+                    </ul>
+                );
+            
+
+            case 3:
+                return(
+                    <ul>        
+                        <li>AMC</li>
+                        <li>Gamestop</li>
+                    </ul>
+                );
+            
+            default: console.log("default")
+        }
+    };
+
+
+    return(
+        <div className="table-of-contents_container">
+            <ul>
+                <h2 className="table-of-contents_header">Table of Contents</h2>
+                <li 
+                    onClick ={() => {
+                        setActiveModule(1);
+                        // subtopics(activeModule);
+                    }}
+                >
+                    Mod 1
+                </li>
+                {activeModule === 1 ? subtopics(activeModule) : null}
+                <li 
+                    onClick ={() => {
+                        setActiveModule(2); 
+                        // subtopics(activeModule);
+                    }}
+                >
+                    Mod 2
+                </li>
+                {activeModule === 2 ? subtopics(activeModule) : null}
+                <li 
+                    onClick ={() => {
+                        setActiveModule(3); 
+                        // subtopics(activeModule);
+                    }}
+                >
+                    Mod 3
+                </li>
+                {activeModule === 3 ? subtopics(activeModule) : null}
+            </ul>
+        </div>
+    );
+}
+
+const Module = () =>(
+
+    <div className="modules__container">
+        <h1 className='modules__header'>Buying your first stock</h1>
+        <p className='modules__text'>
+            Lorem ipsum, dolor sit amet consectetur 
+            adipisicing elit. Nesciunt et ullam voluptatibus dicta.
+            Et aut enim provident quas delectus doloremque laboriosam 
+            dignissimos necessitatibus eum assumenda consequatur, nostrum 
+            accusantium nisi culpa.
+        </p>
+        <img className='modules__img'src="https://i.ytimg.com/vi/if-2M3K1tqk/maxresdefault.jpg" alt="" />
+    </div>
+    
+);
+
+const SubTopic = () => (
+    <div className="sub-topic__container">
+        <h2>Selling your first stock </h2>
+        <p className="sub-topic__text">
+            Lorem ipsum, dolor sit amet consectetur 
+            adipisicing elit. Nesciunt et ullam voluptatibus dicta.
+            Et aut enim provident quas delectus doloremque laboriosam 
+            dignissimos necessitatibus eum assumenda consequatur, nostrum 
+            accusantium nisi culpa.
+        </p>
+        <img className="sub-topic__img" src="https://deadline.com/wp-content/uploads/2019/05/amc.jpg" alt="" />
+    </div>
+);
 
 const ModulesPage = () => {
     return(
         <div className="modules-page">
-            <h1>Hello</h1>
+            <TableOfContents />
+            <div className="content">
+                <Module />
+                <SubTopic />
+            </div>
+            
         </div>
-    )
+    );
 };
 
 
-// wireframe modules page
-// javascript things -----------------------
-// render a list
-// click list
-// where is this list connected to?
-// when list is clicked, show info about topic (how do we want to show this information? will this be a new component?)
-// onClicks, event Handling, on hover
-// smooth scroll to an anchor
-
-
 export default ModulesPage;
+
+
+// style the page fully
+// add an on hover effect and style that
+// create modules and make routing to the modules page dynamic
+// create subtopics
