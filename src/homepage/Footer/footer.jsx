5af04f0250a9f41fc649a0597f2f16c33be91efb
Austin Nguyen <astngyn@gmail.com>
2022-03-02 12:13:22 -0800

    Updated footer, hero, and nav

--- src/homepage/Footer/footer.jsx
+++ src/homepage/Footer/footer.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import './footer.css';
+import logo from '../logo.png'
 
 const Footer = () =>{
     return(
-        <div className="footerSec">
-            <header className="randText"> Footer Placeholder</header>
+        <div className="footer-section">
+            <div className="footer-title">
+                <header className="footer-name"> Tickr &copy;</header>
+                <img src={logo} className="footer-logo"/>
+                <div className="slogan">
+                    <p>The market doesn't stop.</p>
+                </div>
+            </div>
+            
+            <div className="links">
+                <ul>
+                    <li className="foot-link"><a href="#">About Us</a></li>
+                    <li className="foot-link"><a href="#">Discord Bot</a></li>
+                    <li className="foot-link"><a href="#">Contact Us</a></li>
+                    <li className="foot-link"><a href="#">Careers</a></li>
+                </ul>
+            </div>
         </div>
+        
     )
 }
 
