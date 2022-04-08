e864aa4c2429e3f9ad8fd49ef220297715646687
Austin Nguyen <astngyn@gmail.com>
2022-04-08 15:14:34 -0700

    Created modules objects

--- src/homepage/Hero/hero.jsx
+++ src/homepage/Hero/hero.jsx
@@ -3,9 +3,9 @@ import './hero.css'
 import heroImg from '../stock_hero.jpg'
 
 const Hero = () =>(
-    <div className = "hero-container">
+    <div className = "hero_container">
         <img src={heroImg} className="splash" />
-        <h1 className = "hero-caption">The market doesn't stop...</h1>
+        <h1 className = "hero_caption">The market doesn't stop...</h1>
         
     </div>
 )
